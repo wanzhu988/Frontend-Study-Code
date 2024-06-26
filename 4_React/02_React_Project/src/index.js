d3de916bcc387bc664e58e97eaafb8effae5c72d
Wan <wanzhu98@gmail.com>
2024-06-26 22:24:12 +0200

    shangpinhui-resource-unfinished

--- 4_React/02_React_Project/src/index.js
+++ 4_React/02_React_Project/src/index.js
@@ -1,1 +1,15 @@
 //js的入口文件（webpack的编译入口）
+import ReactDOM from 'react-dom/client';
+
+const App = <div>
+    <h1>
+        This is a react project
+    </h1>
+    <p>
+        I have my first react project
+    </p>
+</div>;
+
+//获取根容器
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(App);
